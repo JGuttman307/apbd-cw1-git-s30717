3a0a83812919fa5d7dab4efa755d83997d1390dc
Justyna Guttman <s30717@pjwstk.edu.pl>
2026-03-18 03:26:18 +0100

    Added Sum method to StatisticsHelper class

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,6 +1,16 @@
 ﻿namespace apbd_cw1_git_s30717;
+using System.Collections.Generic;
 
 public class StatisticsHelper
 {
-    
+    //prosta metoda
+    public int Sum(List<int> digitz)
+    {
+        int sum = 0;
+        foreach (var d in digitz)
+        {
+            sum += d;
+        }
+        return sum;
+    }
 }
