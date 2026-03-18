ac495e22229fa5ea982382ddfe61181e2b229e8a
Justyna Guttman <s30717@pjwstk.edu.pl>
2026-03-18 03:22:22 +0100

    Added input handling and storing

--- Program.cs
+++ Program.cs
@@ -4,5 +4,14 @@
     {
         //pierwsza instrukcja/print
         Console.WriteLine("Please enter digits separated by commas: ");
+        
+        string inputs = Console.ReadLine();
+        List<int> digits = inputs.Split(',')
+            .Select(x => int.Parse(x)).ToList();
+        
+        
+        
+        
+        
     }
 }
